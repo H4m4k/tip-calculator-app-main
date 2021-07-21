72888e1a8eaf22244b101aa6821706076770da15
Paweł <pawel.janik.1983@gmail.com>
2021-07-21 20:12:42 +0200

    removed one event listener from bill input, as it was introducing chaos. Proper way to calculate is; enter bill amount, select tip, enter people number

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,19 @@
 const bill = document.querySelector('#amount')
 const people = document.querySelector('#people')
 const buttons = document.querySelectorAll('.tip button')
+const zero = document.querySelector('#zero')
 
 let person_Tip = document.querySelector('#tip_Value')
 let total_Tip = document.querySelector('#total_Value')
 
 let selected_Tip = '0'
 
-bill.addEventListener('input', validate)
 people.addEventListener('input', validate)
+
+buttons.forEach(button => button.addEventListener('click', event => {
+    event.preventDefault();
+    selected_Tip = event.target.textContent.replace('%','')
+}))
 
 
 // ORDER OF CALCULATION 
@@ -24,31 +29,25 @@ ad.4a - display tip amount per person in Tip Amount
 ad.4b - display total amount per person after summing the bill per person and tip per person
 */
 
-buttons.forEach(button => button.addEventListener('click', selectTip))
 
 
-function selectTip(event) {
-   event.preventDefault();
-   selected_Tip = event.target.textContent.replace('%','')
-   return console.log(selected_Tip)
-}
 
-function personal_Tip() {
+function personal_Tip(percent) {
     if (people.value === '0' || selected_Tip === '0' ) {
         return person_Tip.textContent = '0.00'
     } else {
-        return ((selectTip()/100)*bill.value).toFixed(2)
+        return ((percent/100)*bill.value).toFixed(2)
     }
 
 }
 
 function personal_Bill() {
     if (people.value === '0') {
-        // remove class off from 'Can't be zero'
+        zero.classList.remove('off')
         people.classList.add('red_Outline')
         return total_Tip.textContent = '0.00'
     } else {
-         // add class off to 'Can't be zero'
+         zero.classList.add('off')
          people.classList.remove('red_Outline')
          return (bill.value/people.value).toFixed(2)
     }
@@ -56,8 +55,9 @@ function personal_Bill() {
 }
 
 function validate() {
-    person_Tip.textContent = `$${personal_Tip()}`
-    total_Tip.textContent = `$${personal_Bill()}`
+    console.log(selected_Tip)
+    person_Tip.textContent = `$${personal_Tip(selected_Tip)}`
+    total_Tip.textContent = `$${personal_Bill() }`
     return console.log(people.value)
 }
 
