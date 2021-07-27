4e05bfc6cf9746d7a59e4af9b2638802773a0cb3
Paweł <pawel.janik.1983@gmail.com>
2021-07-27 13:40:09 +0200

    Fixed the display of tip and bill per person amount

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,38 @@
 const bill = document.querySelector('#amount')
 const people = document.querySelector('#people')
 const buttons = document.querySelectorAll('.tip button')
-const zero = document.querySelector('#zero')
+const cant_Zero = document.querySelector('#warning')
 const reset_Btn = document.querySelector('#reset')
 
 let person_Tip = document.querySelector('#tip_Value')
 let person_Bill = document.querySelector('#total_Value')
 
-let selected_Tip = '0';
-
+let selected_Tip = 0;
+let last_Target = '';
 
 people.addEventListener('input', validate)
 
+
+
+
+
+
+//-----------------// AD. 2 //-----------------//
 buttons.forEach(button => button.addEventListener('click', event => {
     event.preventDefault();
-    selected_Tip = event.target.textContent.replace('%','');
-    validate();
+    selected_Tip = Number.parseInt(event.target.textContent);
+    personal_Tip();
 }))
 
+buttons.forEach(button => {button.addEventListener('focus', event => {
+    event.target.style.backgroundColor = '#26c2ad'
+    event.target.style.color = '#00494d'    
+})
+})
+
+
+
+//-----------------// AD. 5 //-----------------//
 reset_Btn.addEventListener('click', reset)
 
 // ORDER OF CALCULATION 
@@ -26,9 +41,10 @@ reset_Btn.addEventListener('click', reset)
 2. user selects, a tip
 3. user enters number of people 
 4. display outcome
+5. reset
 
-ad.2 - calculate the total amount of tip from the bill
-ad.3 - divide the bill by number of people, divide the tip, by the number of people
+ad.2 - calculate the bill per person, divide bill by people number
+ad.3 - calculate the tip from bill, user selects TIP percentage
 ad.4a - display tip amount per person in Tip Amount
 ad.4b - display total amount per person after summing the bill per person and tip per person
 */
@@ -37,24 +53,23 @@ ad.4b - display total amount per person after summing the bill per person and ti
 
 
 function personal_Tip(percent = '0') {
-    if (people.value === '0' || selected_Tip === '0' ) {
+    if (personal_Bill.textContent === '0.00') {
         return person_Tip.textContent = '0.00'
     } else {
-        tip =  ( ( (percent/100)*bill.value )/people.value  ).toFixed(2)
-        return tip
+        return  ( ( (percent/100)*bill.value )/people.value  ).toFixed(2)
     }
 
 }
 
 function personal_Bill() {
     if (people.value === '0') {
-        zero.classList.remove('off')
+        cant_Zero.classList.remove('off')
         people.classList.add('red_Outline')
         return person_Bill.textContent = '0.00'
     } else {
-         zero.classList.add('off')
+         cant_Zero.classList.add('off')
          people.classList.remove('red_Outline')
-         return ((bill.value/people.value)).toFixed(2)
+         return (bill.value/people.value).toFixed(2)
     }
 }
 
@@ -63,16 +78,20 @@ function sum() {
 }
 
 function validate() {
-    console.log(selected_Tip)
-    person_Tip.textContent = `$${personal_Tip(selected_Tip)}`
-    person_Bill.textContent = `$${sum().toFixed(2)}`
-    return console.log(people.value)
+    if(people.value === '0') {
+
+    } else {
+        person_Tip.textContent = `$${personal_Tip(selected_Tip)}`
+        person_Bill.textContent = `$${sum().toFixed(2)}`
+        return console.log(people.value)
+    }
 }
 
 function reset() {
-    bill.value = '0'
-    people.value = '0'
-    personal_Tip();
-    personal_Bill()
+    bill.value = 0;
+    people.value = 0;
+    selected_Tip = 0;
+    person_Tip.textContent =`$${'0.00'}`;
+    person_Bill.textContent = `$${'0.00'}`;
     return 0;
 }
